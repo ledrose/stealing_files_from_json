fedc1b083bd1ec7c0a6c2b7a8c9f26b20ff79a97
ledrose <embre@bk.ru>
2020-04-19 18:38:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#openSB").click(function (e) { 
+    e.preventDefault();
+    $("#sidebar").css("margin-left", "0");
+});
+
+$("#closeSB").click(function (e) { 
+    e.preventDefault();
+    $("#sidebar").css("margin-left", "-250px");
+});
+
+$(document).ready(function () {
+    
+});
