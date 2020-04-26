9617b448be84126f0e924deb95795b96e39de4d0
ledrose <embre@bk.ru>
2020-04-26 17:17:02 +0700

    HalfDone Sidebar

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ $("#closeSB").click(function (e) {
     $("#sidebar").css("margin-left", "-250px");
 });
 
-$(document).ready(function () {
-    
-});
+// $(document).ready(function () {
+    var WWidth=screen.width;
+    console.log(WWidth);
+    if (WWidth<=767) {
+        $(".curDevice1").addClass("selected");
+        $(".curDevice2").removeClass("selected");
+        $(".curDevice3").removeClass("selected");
+    }
+    if (WWidth>767 && WWidth<1000) {
+        $(".curDevice2").addClass("selected");
+        $(".curDevice1").removeClass("selected");
+        $(".curDevice3").removeClass("selected");
+    }
+    if (WWidth>=1000) {
+        $(".curDevice3").addClass("selected");
+        $(".curDevice2").removeClass("selected");
+        $(".curDevice1").removeClass("selected");
+    }
+// });
